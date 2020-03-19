bbcd4f6b9025c668667a5242c555e6e2371d98c9
Eshan Agarwal <eshanagarwal12@gmail.com>
2020-03-19 10:26:57 +0530

    Update plant_village_test.py

--- tensorflow_datasets/image/plant_village_test.py
+++ tensorflow_datasets/image/plant_village_test.py
@@ -26,7 +26,7 @@ from tensorflow_datasets.image import plant_village
 
 class PlantVillageTest(testing.DatasetBuilderTestCase):
   DATASET_CLASS = plant_village.PlantVillage
-  SPLITS = {"train": 38}
+  SPLITS = {"train": 35}
 
 
 if __name__ == "__main__":
