a934063ce4f1b9391b399d230dc6923cec850274
Eshan Agarwal <eshanagarwal12@gmail.com>
2020-03-27 11:16:21 +0530

    Update dataset_builder_test.py

--- tensorflow_datasets/core/dataset_builder_test.py
+++ tensorflow_datasets/core/dataset_builder_test.py
@@ -20,9 +20,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import sys
-sys.path.insert(0,'.')
-
 import os
 
 from absl.testing import absltest
